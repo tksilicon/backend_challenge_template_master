157f736f343db7dc8b4f58499a942338aa6e0062
tksilicon <frankgod02@gmail.com>
2019-08-15 19:50:38 +0100

    jest testing mysql

--- src/_tests_/controller.test.js
+++ src/_tests_/controller.test.js
@@ -39,8 +39,7 @@ describe('Test the root path', () => {
       });
   });
 });
-
-/** describe('GET /departments', () => {
+describe('GET /departments', () => {
   test('It responds with an array of departments', async () => {
     const response = await request(server).get('/departments');
     expect(response.body).toEqual(departments);
@@ -51,4 +50,4 @@ describe('Test the root path', () => {
 
     expect(response.statusCode).toBe(200);
   });
-});*/
+});
