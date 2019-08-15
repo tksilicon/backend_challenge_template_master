080f51d2da69fdf6a831576fb8eb4f105c111ac6
tksilicon <frankgod02@gmail.com>
2019-08-16 00:22:25 +0100

    webpack production

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -15,7 +15,7 @@ module.exports = {
     logging: false,
   },
   production: {
-    use_env_variable: DATABASE_URL,
+    use_env_variable: process.env.DATABASE_URL,
     dialect: 'mysql',
     logging: false,
   },
