034a677aabe4491bfc8cf80c64e2a05d3f8c045d
tksilicon <frankgod02@gmail.com>
2019-08-16 00:19:27 +0100

    webpack production

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    use_env_variable: DATABASE_URL,
+    use_env_variable: process.env.DATABASE_URL,
     dialect: 'mysql',
     logging: false,
   },
