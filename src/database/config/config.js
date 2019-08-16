bf28326b8ebeb898d3b0697243476a606860237c
tksilicon <frankgod02@gmail.com>
2019-08-16 03:36:33 +0100

    webpack production

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -12,10 +12,10 @@ module.exports = {
     "logging": false,
   },
   "test": {
-    "username": process.env.TEST_DB_USER,
-    "password": process.env.TEST_DB_PASS,
-    "database": process.env.TEST_DB_NAME,
-    "host": process.env.TEST_DB_HOST,
+   "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST
     "dialect": "mysql",
     "logging": false,
   },
