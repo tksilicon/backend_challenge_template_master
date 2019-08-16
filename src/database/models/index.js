ce4e533a34ee1bd47e299b8c8a94675f3a399423
tksilicon <frankgod02@gmail.com>
2019-08-16 02:46:53 +0100

    webpack production

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -5,22 +5,18 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-require('dotenv').config();
+
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-const connString = process.env.DATABASE_URL;
+
 
 let sequelize;
 if (config.use_env_variable) {
-  /** sequelize = new Sequelize(process.env[config.use_env_variable], config);**/
-	  sequelize =  new Sequelize(connString, { 
-		  dialect: 'mysql'
-	  });
-  
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
